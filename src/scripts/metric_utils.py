e59cbbade6109c144c96bb83d750450ce444b883
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-04-07 20:06:12 +0300

    Add r2, f1 and recall metrics

--- src/scripts/metric_utils.py
+++ src/scripts/metric_utils.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score, r2_score, recall_score, roc_auc_score
 
 
 def compute_metrics(predictions, truths):
@@ -7,10 +7,17 @@ def compute_metrics(predictions, truths):
     """
 
     assert len(predictions) == len(truths)
-    score = roc_auc_score(truths, predictions)
+    rounded = [round(x) for x in predictions]
+    score = roc_auc_score(truths, rounded)
+    r2 = r2_score(truths, rounded)
+    f1 = f1_score(truths, rounded)
+    recall = recall_score(truths, rounded)
 
     to_return = {
         "lb": round(score, 4),
+        "r2": round(r2, 4),
+        "f1": round(f1, 4),
+        "recall": round(recall, 4),
     }
 
     return to_return
